176a5dd4a298a1bc0abfc4b0049cc588cfd58ee0
tangyunfei <451202165@qq.com>
2021-03-02 11:13:26 +0800

    add:首页加载

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import {createApp} from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 
 const app = createApp(App);
-app.use(store)
-app.use(router)
-app.use(Antd)
+app.use(store);
+app.use(router);
+app.use(Antd);
 router.isReady().then(() => {
-  app.mount('#app')
+  const body = document.querySelector('body');
+  const preloader = document.querySelector('.preloader');
+  (preloader as any).addEventListener('transitionend', () => {
+    (preloader as any).className = 'preloader-hidden';
+  });
+
+  (preloader as any).className +=
+    ' preloader-hidden-add preloader-hidden-add-active';
+  (body as any).style.overflow = '';
+  app.mount('#app');
 });
 export default app;
